3d80f6ace7ad83c51b591664c64f30386582aed3
josefine-krarup <joskr0005@stud.kea.dk>
2026-05-28 14:55:12 +0200

    aicomments-done

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -4,7 +4,7 @@ const Loader = ({ text }) => {
   return (
     <div className="fixed inset-0 bg-black flex flex-col items-center justify-center z-50 gap-4">
       <Image src="/assets/loader/madbars.gif" alt="loading" width={50} height={50} />
-
+      // AI blev brugt til sparring om, hvordan teksten kun vises hvis text findes.
       {text && <p className="text-center text-sm">{text}</p>}
     </div>
   );
